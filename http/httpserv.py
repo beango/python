918ce6ebc335aa36cd825b297180fff2de635fcb
beango <6588617@gmail.com>
2014-04-02 16:59:34 +0800

    Signed-off-by: beango <6588617@gmail.com>

--- http/httpserv.py
+++ http/httpserv.py
@@ -4,7 +4,7 @@
 #
 # sockchat serv
 
-import os, socket
+import sys, os, socket, thread
 import datetime
 
 class httpserv:
@@ -14,58 +14,59 @@ class httpserv:
 		self.GMT_FORMAT = '%a, %d %b %Y %H:%M:%S GMT'
 
 	def run(self):
-		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		sock.bind(('localhost',12345))
-		sock.listen(5)
+		try:
+			self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			self.sock.bind(('localhost',12345))
+			self.sock.listen(5)
 
-		while 1:
-			conn, address = sock.accept()
 			while 1:
-				data = conn.recv(2048)
-				if not data:break
-				arr_head = data.split('\r\n')
-				request_url = arr_head[0].split(' ')[1].split('?')[0]
-				#print data
-				filepath = "content" + request_url
-				now = datetime.datetime.utcnow()
-				expires = datetime.timedelta(seconds=60)
-				
-				if os.path.isfile(filepath):
-					response_code = "200 OK"
-					content = open(filepath, 'rb').read()
-				else:
-					response_code = "400 Not Found"
-					content = "<h1>Page not found!</h1>"
+				conn, client = self.sock.accept()
+				thread.start_new_thread(self.handlerconn,((conn,self.sock),))
+		except socket.error, msg:
+			print msg
+			sys.exit()
+		finally:
+			self.sock.close()
 
-				response = '''HTTP/1.1 %s
-					Server: %s
-					Date: %s
-					Expires: %s
-					Content-Type: %s;charset=utf-8
-					Content-Length: %s
-					Connection: keep-alive
+	def handlerconn(self, _args):
+		conn, sock = _args[0], _args[1]
+		while 1:
+			data = conn.recv(2048)
+			if not data:break
+			arr_head = data.split('\r\n')
+			request_url = arr_head[0].split(' ')[1].split('?')[0]
+			print request_url
+			filepath = "content" + request_url
+			now = datetime.datetime.utcnow()
+			expires = datetime.timedelta(seconds=60)
+			
+			if os.path.isfile(filepath):
+				response_code = "200 OK"
+				content = open(filepath, 'rb').read()
+			else:
+				response_code = "400 Not Found"
+				content = "<h1>Page not found!</h1>"
 
-					%s''' % (
-					response_code,
-					self.server_name, 
-					now.strftime(self.GMT_FORMAT), 
-					(now + expires).strftime(self.GMT_FORMAT),
-					self.getAccept(request_url),
-					len(content),
-					content
-				)
-				conn.send(response)
-				if request_url =="/bundles/blog-common.css":
-					print response
-				break
-			conn.close()
-		sock.close()
+			response = '''HTTP/1.1 %s\r\nServer: %s\r\nDate: %s\r\nExpires: %s\r\nContent-Type: %s\r\nContent-Length: %s
+Connection: keep-alive\r\n\r\n%s''' % (
+				response_code,
+				self.server_name, 
+				now.strftime(self.GMT_FORMAT), 
+				(now + expires).strftime(self.GMT_FORMAT),
+				self.getAccept(request_url),
+				len(content),
+				content
+			)
+			conn.send(response)
+			break
+		conn.close()
+	
 	def getAccept(self, _url):
-		if _url.endswith(".css"):return "text/css"
-		if _url.endswith(".js"):return "text/javascript"
+		if _url.endswith(".css"):return "text/css; charset=utf-8"
+		if _url.endswith(".js"):return "application/javascript"
 		if _url.endswith(".gif"):return "image/gif"
 		if _url.endswith(".png"):return "image/png"
 		if _url.endswith(".jpg"):return "image/jpg"
-		return "text/html";
+		return "text/html;charset=utf-8";
 serv = httpserv()
 serv.run()
